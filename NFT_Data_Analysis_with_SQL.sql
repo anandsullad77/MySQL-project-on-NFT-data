6c1624831d4b85a6652114009521da8ec7b31734
Anand Sullad <107276933+anandsullad77@users.noreply.github.com>
2024-01-18 18:24:00 +0530

    Update NFT_Data_Analysis_with_SQL.sql
    Removed extra junk code

--- NFT_Data_Analysis_with_SQL.sql
+++ NFT_Data_Analysis_with_SQL.sql
@@ -28,7 +28,7 @@ SELECT
 FROM
     pricedata
 GROUP BY name
-ORDER BY name;
+ORDER BY average_price;
 
 -- 5.0 Return each day of the week and the number of sales that occurred on that day of the week, as well as the average price in ETH. Order by the count of transactions in ascending order.
 SELECT 
@@ -80,12 +80,12 @@ ORDER BY bucket;
 -- a query that has the lowest price each NFT was bought for and the status column saying “lowest”. The table should have a name column, 
 -- a price column called price, and a status column. Order the result set by the name of the NFT, and the status, in ascending order. 
 SELECT 
-    name, MAX(eth_price) AS price, CONCAT('Highest') AS status
+    name, MAX(eth_price) AS price, 'Highest' AS status
 FROM
     pricedata
 GROUP BY name 
 UNION SELECT 
-    name, MIN(eth_price) AS price, CONCAT('Lowest') AS status
+    name, MIN(eth_price) AS price, 'Lowest' AS status
 FROM
     pricedata
 GROUP BY name
@@ -120,8 +120,8 @@ SELECT
     COUNT(*) AS sum_of_sales_volume
 FROM
     pricedata
-GROUP BY sale_year , sale_month
-order by sale_year , sale_month; 
+GROUP BY YEAR(event_date), MONTH(event_date)
+ORDER BY YEAR(event_date), MONTH(event_date); 
 
 
 -- 12.0 Count how many transactions the wallet "0x1919db36ca2fa2e15f9000fd9cdc2edcf863e685"had over this time period.
